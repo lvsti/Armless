179416f0c8b65a0f3c28ee2eda8e74f5709fc577
Tamas Lustyik <elveestei@gmail.com>
2020-10-10 22:05:27 +0200

    Show a status bar spinner when processing

--- UnArm/MainStatusBar.swift
+++ UnArm/MainStatusBar.swift
@@ -12,10 +12,22 @@ struct MainStatusBar: View {
 
     var body: some View {
         HStack {
+            if viewModel.state.isProcessing {
+                Color.clear
+                    .frame(width: 20, height: 24)
+                Spacer()
+            }
+            
             Text(statusText)
                 .font(.system(size: 12.0, weight: .regular, design: .default))
                 .foregroundColor(Color(.secondaryLabelColor))
                 .frame(height: 24, alignment: .center)
+
+            if viewModel.state.isProcessing {
+                Spacer()
+                Spinner(isAnimating: Binding(get: { true }, set: { _ in }), style: .spinning, controlSize: .small)
+                    .padding(EdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4))
+            }
         }
     }
 
