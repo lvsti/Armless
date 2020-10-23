816f9de42d7c84f26cd9dbbee861aef4a4a05c26
Tamas Lustyik <elveestei@gmail.com>
2020-10-23 19:15:18 +0200

    Moved view logic to VM

--- UnArm/MainStatusBar.swift
+++ UnArm/MainStatusBar.swift
@@ -30,23 +30,4 @@ struct MainStatusBar: View {
             }
         }
     }
-
-    private var statusText: String {
-        guard !viewModel.state.scanResults.isEmpty else { return "No files" }
-
-        let (eligibleCount, originalSize, reducedSize) = viewModel.state.scanResults.reduce((0, Int64(0), Int64(0))) { acc, next in
-            guard next.slices.count > 1 else { return acc }
-            let savings = next.slices.reduce(Int64(0)) { acc, next in acc + next.value } - next.slices[viewModel.state.sliceTypeForCurrentArch]!
-            return (acc.0 + 1, acc.1 + next.originalSize, acc.2 + savings)
-        }
-
-        return "Estimated savings: \(Self.sizeFormatter.string(fromByteCount: originalSize - reducedSize)) " +
-            "of \(Self.sizeFormatter.string(fromByteCount: originalSize)) in \(eligibleCount) binaries"
-    }
-
-    private static let sizeFormatter: ByteCountFormatter = {
-        let fmt = ByteCountFormatter()
-        fmt.countStyle = .file
-        return fmt
-    }()
 }
--- UnArm/MainViewModel.swift
+++ UnArm/MainViewModel.swift
@@ -46,6 +46,24 @@ struct MainViewState {
     var isStartButtonEnabled: Bool {
         !isProcessing && scanResults.contains(where: { $0.isEligible })
     }
+    var statusText: String {
+        guard !scanResults.isEmpty else { return "No files" }
+
+        let (eligibleCount, originalSize, reducedSize) = scanResults.reduce((0, Int64(0), Int64(0))) { acc, next in
+            guard next.slices.count > 1 else { return acc }
+            let savings = next.slices.reduce(Int64(0)) { acc, next in acc + next.value } - next.slices[sliceTypeForCurrentArch]!
+            return (acc.0 + 1, acc.1 + next.originalSize, acc.2 + savings)
+        }
+
+        return "Estimated savings: \(Self.sizeFormatter.string(fromByteCount: originalSize - reducedSize)) " +
+            "of \(Self.sizeFormatter.string(fromByteCount: originalSize)) in \(eligibleCount) binaries"
+    }
+
+    private static let sizeFormatter: ByteCountFormatter = {
+        let fmt = ByteCountFormatter()
+        fmt.countStyle = .file
+        return fmt
+    }()
 }
 
 enum MainViewInput {
